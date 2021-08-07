1e206cc1a6aa024cb71c6a0fc3a8e76954a40282
Sayed <nuruddinsayeed@gmail.com>
2021-08-08 03:26:47 +0600

    about contat services pages added

--- car_deal/pages/views.py
+++ car_deal/pages/views.py
@@ -2,4 +2,24 @@ from django.shortcuts import render
 
 
 def index(request):
+    """Renders Home page"""
+
     return render(request, "pages/index.html")
+
+
+def about(request):
+    "Renders About page"
+
+    return render(request, "pages/about.html")
+
+
+def services(request):
+    "Renders services page"
+
+    return render(request, "pages/services.html")
+
+
+def contact(request):
+    "Renders contact page"
+
+    return render(request, "pages/contact.html")
--- car_deal/static/js/app.js
+++ car_deal/static/js/app.js
@@ -84,45 +84,45 @@ $(function () {
 
 
     // Header shrink while page scroll
-    // adjustHeader();
-    // doSticky();
-    // placedDashboard();
-    // $(window).on('scroll', function () {
-    //     adjustHeader();
-    //     doSticky();
-    //     placedDashboard();
-    // });
+    adjustHeader();
+    doSticky();
+    placedDashboard();
+    $(window).on('scroll', function () {
+        adjustHeader();
+        doSticky();
+        placedDashboard();
+    });
 
-    // // Header shrink while page resize
-    // $(window).on('resize', function () {
-    //     adjustHeader();
-    //     doSticky();
-    //     placedDashboard();
-    // });
+    // Header shrink while page resize
+    $(window).on('resize', function () {
+        adjustHeader();
+        doSticky();
+        placedDashboard();
+    });
 
-    // function adjustHeader() {
-    //     var windowWidth = $(window).width();
-    //     if (windowWidth > 0) {
-    //         if ($(document).scrollTop() >= 100) {
-    //             if ($('.header-shrink').length < 1) {
-    //                 $('.sticky-header').addClass('header-shrink');
-    //             }
-    //             if ($('.do-sticky').length < 1) {
-    //                 $('.company-logo img').attr('src', 'static/img/logos/black-logo.png');
-    //             }
-    //         }
-    //         else {
-    //             $('.sticky-header').removeClass('header-shrink');
-    //             if ($('.do-sticky').length < 1 && $('.fixed-header').length == 0 && $('.fixed-header2').length == 0) {
-    //                 $('.company-logo img').attr('src', 'static/img/logos/logo.png');
-    //             } else {
-    //                 $('.company-logo img').attr('src', 'static/img/logos/black-logo.png');
-    //             }
-    //         }
-    //     } else {
-    //         $('.company-logo img').attr('src', 'static/img/logos/black-logo.png');
-    //     }
-    // }
+    function adjustHeader() {
+        var windowWidth = $(window).width();
+        if (windowWidth > 0) {
+            if ($(document).scrollTop() >= 100) {
+                if ($('.header-shrink').length < 1) {
+                    $('.sticky-header').addClass('header-shrink');
+                }
+                if ($('.do-sticky').length < 1) {
+                    $('.company-logo img').attr('src', '/static/img/logos/black-logo.png');
+                }
+            }
+            else {
+                $('.sticky-header').removeClass('header-shrink');
+                if ($('.do-sticky').length < 1 && $('.fixed-header').length == 0 && $('.fixed-header2').length == 0) {
+                    $('.company-logo img').attr('src', '/static/img/logos/logo.png');
+                } else {
+                    $('.company-logo img').attr('src', '/static/img/logos/black-logo.png');
+                }
+            }
+        } else {
+            $('.company-logo img').attr('src', '/static/img/logos/black-logo.png');
+        }
+    }
 
     function doSticky() {
         if ($(document).scrollTop() > 40) {
