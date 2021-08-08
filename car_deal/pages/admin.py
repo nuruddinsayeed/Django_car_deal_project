c563ed1d031e23fb49abfa5a99cf9613df6ecb27
Sayed <nuruddinsayeed@gmail.com>
2021-08-08 19:17:04 +0600

    team data dynamically added from database and team admin page customized

--- car_deal/pages/admin.py
+++ car_deal/pages/admin.py
@@ -1,4 +1,36 @@
 from django.contrib import admin
 from .models import Team
+from django.utils.html import format_html
 
-admin.site.register(Team)
+
+class TeamAdmin(admin.ModelAdmin):
+    """Customize team view in admin page"""
+
+    def thumbnil(self, object):
+        """This will recieve model data as object here
+        So we will retrieve photo from it and retrun as html <IMG/>
+        and also this thumbnil will be availabel as obj attribute"""
+
+        return format_html(
+            '<img src="{}" width="40" style="border-radius: 50px;"/>'.format(
+                object.photo.url  # this will be available inside src{}
+            )
+        )
+
+    def name(self, object):
+        "return full name"
+        return (object)
+
+    # lest change thumnil title(description) from thumb-> photo
+    thumbnil.short_description = "photo"
+
+    list_display = ('id', 'thumbnil', 'name',
+                    'designation', 'created_at',)
+    list_display_links = ('id', 'thumbnil', 'name',)
+    # now lets create a search filds for the admin panel
+    search_fields = ('first_name', 'last_name', 'designation',)
+    # lets create filter option for the admin panel
+    list_filter = ('designation', )
+
+
+admin.site.register(Team, TeamAdmin)
--- car_deal/pages/views.py
+++ car_deal/pages/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
+from .models import Team
+
+
+def get_teams_info():
+    return {"teams": Team.objects.all()}
 
 
 def index(request):
     """Renders Home page"""
+    data = get_teams_info()
 
-    return render(request, "pages/index.html")
+    return render(request, "pages/index.html", data)
 
 
 def about(request):
     "Renders About page"
+    data = get_teams_info()
 
-    return render(request, "pages/about.html")
+    return render(request, "pages/about.html", data)
 
 
 def services(request):
