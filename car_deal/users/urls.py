23222e6a879bd2b2751913f7a840d70eb1b2da6c
Sayed <nuruddinsayeed@gmail.com>
2021-08-10 05:12:00 +0600

    login register page template added:

--- car_deal/users/urls.py
+++ car_deal/users/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path("", views.dashboard, name='dashboard'),
+    path("login/", views.login, name='login'),
+    path("register/", views.register, name='register'),
 ]
--- car_deal/users/views.py
+++ car_deal/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    """Render Login Template"""
+
+    return render(request, "users/login.html")
+
+
+def register(request):
+    """Render register Template"""
+
+    return render(request, "users/register.html")
+
+
+def dashboard(request):
+    """Render dashboard Template"""
+
+    return render(request, "users/dashboard.html")
