795cccc772c8097d4fc379a2112583d1fd7f6792
Sayed <nuruddinsayeed@gmail.com>
2021-08-09 12:30:36 +0600

    search page added with car search functionality

--- car_deal/cars/urls.py
+++ car_deal/cars/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.CarsView.as_view(), name='cars'),
     path('<int:pk>/', views.CarDetailView.as_view(), name='car'),
+    path('search/', views.CarSearchResult.as_view(), name='search'),
 ]
--- car_deal/cars/views.py
+++ car_deal/cars/views.py
@@ -22,3 +22,24 @@ class CarDetailView(DetailView):
     template_name = "cars/car.html"
     model = Car
     context_object_name = 'car'
+
+
+class CarSearchResult(ListView):
+    """Renders Search result of cars"""
+
+    template_name = "cars/search.html"
+    # model = Car
+    paginate_by = 3
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        """Overriding get queryset method"""
+
+        cars = Car.objects.all()
+
+        keyword = self.request.GET.get('keyword')
+        if keyword:
+            cars = Car.objects.order_by(
+                '-created_date').filter(description__icontains=keyword)
+
+        return cars
