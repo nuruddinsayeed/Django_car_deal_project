0478eb871b60edf449ec83eca99353be34be545a
Sayed <nuruddinsayeed@gmail.com>
2021-08-14 07:06:03 +0600

    Customer Inquiry Model added

--- car_deal/inquiry/admin.py
+++ car_deal/inquiry/admin.py
@@ -1,3 +1,15 @@
+from inquiry.models import Inquiry
 from django.contrib import admin
 
-# Register your models here.
+
+class InqueiryAdmin(admin.ModelAdmin):
+    """Customize inquiry for the admin page display"""
+
+    list_display = ("id", "name", "car", "created_at")
+    list_display_links = ("id", "name", "car")
+    search_fields = ("name",)
+    list_per_page = 25
+    list_filter = ("car", )
+
+
+admin.site.register(Inquiry, InqueiryAdmin)
--- car_deal/inquiry/models.py
+++ car_deal/inquiry/models.py
@@ -1,1 +1,27 @@
 from django.db import models
+
+from cars.models import Car
+from users.models import CustomUser
+
+
+class Inquiry(models.Model):
+    """Define Inquiry Model Database"""
+
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.SET_NULL, blank=True, null=True)
+
+    name = models.CharField(max_length=150)
+    customer_query = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, blank=True)
+    phone = models.CharField(max_length=14, blank=True)
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Inquiries"
+
+    def __str__(self):
+        return f"{self.name}: ({self.car.car_title})"
